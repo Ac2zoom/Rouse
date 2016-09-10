b5b8e7b971139a4c68962932993657cfe3111661
Ac2zoom <ac2zoom@yahoo.com>
2016-09-10 16:11:43 -0400

    Finished registration and login functions

--- rouse.php
+++ rouse.php
@@ -6,8 +6,30 @@ $db = $m->rouse;
 $collection = $db->users;
 
 // Add new user to database
-function addUser($name, $email, $password_hash, $goal, $passion) {
+function register($first_name, $last_name, $email, $password_hash, $phone,
+                  $interests, $goals) {
+    $a = array("email" => $email);
+    $cursor = $GLOBALS['collection']->find($a);
+    if (count($cursor) === 0) {
+        $a = array("first_name" => $first_name, "last_name" => $last_name, "email" => $email,
+                   "password_hash" => $password_hash, "phone" => $phone, "interests" => $interests,
+                   "goals" => $goals);
+        $GLOBALS['collection']->insert($a);
+        return "Success";
+    } else {
+        return "Failure: User Already Exists";
+    }
+}
 
+// Get user information
+function login($email, $password_hash) {
+    $a = array("email" => $email, "password_hash" => $password_hash);
+    $cursor = $GLOBALS['collection']->find($a);
+    if (count($cursor) === 0) {
+        return "Incorrect Password";
+    } else {
+        return $cursor[0];
+    }
 }
 
 // Add time to call for next day
@@ -22,10 +44,5 @@ function changePassion() {
 
 // Change goal
 function changeGoal() {
-
-}
-
-// Get user information
-function getUserInfo() {
 
 }
